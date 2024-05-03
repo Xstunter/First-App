593d7f4be4c20c0a3941b1a17167101ab46bd850
Bogdan <bdatsenko2@ukr.net>
2024-05-03 22:18:49 +0300

    Change Angular.UI

--- MVC/Angular.UI/src/app/app.component.ts
+++ MVC/Angular.UI/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BoardComponent } from './components/boards/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
