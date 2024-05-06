23b1653bf655740a659794c66bf2bc185a5f2baa
Bogdan <bdatsenko2@ukr.net>
2024-05-06 17:08:53 +0300

    Added Modal Form for CreateBoard

--- MVC/Angular.UI/src/app/components/boards/board.component.ts
+++ MVC/Angular.UI/src/app/components/boards/board.component.ts
@@ -1,20 +1,31 @@
-import {Component} from '@angular/core'
+import { Component } from '@angular/core'
 import { IBoard } from '../../models/board'
 import { HttpService } from '../../http.sevice';
 import { CreateBoardRequest } from '../../models/requests/createboard.request';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-board',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     templateUrl: './board.component.html'
 })
 
 export class BoardComponent {
+
     board: IBoard = { boardId: 0, name: '', description: '' };
+    showCreateBoardModal = false;
 
     constructor(private httpService: HttpService) {}
+
+    openCreateBoardModal() {
+        this.showCreateBoardModal = true;
+    }
+    
+      closeCreateBoardModal() {
+        this.showCreateBoardModal = false;
+    }
 
     getBoardNgOnInit(){
         this.httpService.getBoard(this.board.boardId).subscribe(result=>{
@@ -28,10 +39,12 @@ export class BoardComponent {
         this.httpService.createBoard(board).subscribe(result=>{
             this.board.boardId=result;
             console.log(result);
-        })          
+        })
+        this.closeCreateBoardModal();          
     }
     deleteBoardNgOnInit(){
         let isDeleted : Boolean = false;
+
         this.httpService.deleteBoard(this.board.boardId).subscribe(result=>{
             isDeleted=result;
             console.log(result);
